bb1123f3294e0ff10e4947089c82ab05c477971a
prateek1190 <prateek.dixit@flipkart.com>
2015-01-29 19:34:46 +0530

    adding javadocs

--- library/src/main/java/com/flipkart/layoutengine/ImageLoaderCallBack.java
+++ library/src/main/java/com/flipkart/layoutengine/ImageLoaderCallBack.java
@@ -6,6 +6,16 @@ import android.graphics.Bitmap;
  * Created by prateek.dixit on 1/29/15.
  */
 public interface ImageLoaderCallBack {
+
+    /**
+     * Used for asynchronous image loading and when response is not null
+     * @param bitmap
+     */
     public void onResponse(Bitmap bitmap);
+
+    /**
+     * Used when response returned from client is due to an error
+     * @param errorMessage
+     */
     public void onErrorReceived(String errorMessage);
 }
