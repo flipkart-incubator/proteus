99975d690da314efd1651e074adf4dd6b225e427
Aditya Sharat <aditya.sharat@flipkart.com>
2015-04-02 12:37:46 +0530

    #5 | SimpleDataAndViewParser implementation

--- library/src/main/java/com/flipkart/layoutengine/parser/SimpleDataAndViewParser.java
+++ library/src/main/java/com/flipkart/layoutengine/parser/SimpleDataAndViewParser.java
@@ -4,18 +4,32 @@ import android.view.View;
 
 import org.json.JSONObject;
 
+import java.util.Map;
+import java.util.Objects;
+
 /**
- * A DataAndViewParser which
- * Created by Aditya Sharat on 02-04-2015.
+ * A {@link com.flipkart.layoutengine.parser.DataAndViewParser} implementation to update the data
+ * associated with a {@link android.view.View} built using a {@link com.flipkart.layoutengine.builder.LayoutBuilder}.
+ *
+ * @author Aditya Sharat {@literal <aditya.sharat@flipkart.com>}
  */
 public class SimpleDataAndViewParser implements DataAndViewParser {
+
+    private Map<String, Object> dataModelToViewModelTree;
+    private View view;
+
+    public SimpleDataAndViewParser (View view, Map<String, Object> dataModelToViewModelTree) {
+        this.view = view;
+        this.dataModelToViewModelTree = dataModelToViewModelTree;
+    }
+
     @Override
     public View getView() {
-        return null;
+        return this.view;
     }
 
     @Override
     public View updateView(JSONObject data) {
-        return null;
+        return this.view;
     }
 }
