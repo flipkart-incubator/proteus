50151fc340de12c0d3d1f0dc7086cd9771ed739f
Aditya Sharat <aditya.sharat@flipkart.com>
2015-04-08 16:45:13 +0530

    #5 | new Uitls class for the utility methods

---
+++ library/src/main/java/com/flipkart/layoutengine/toolbox/Utils.java
@@ -0,0 +1,29 @@
+package com.flipkart.layoutengine.toolbox;
+
+import android.util.Log;
+
+import com.flipkart.layoutengine.provider.Provider;
+import com.google.gson.JsonElement;
+
+/**
+ * Created by Aditya Sharat on 08-04-2015.
+ */
+public class Utils {
+
+    private static final String TAG = Utils.class.getSimpleName();
+
+    public static JsonElement getElementFromData(Character PREFIX, JsonElement element, Provider dataProvider, int childIndex) {
+        if (element.isJsonPrimitive()) {
+            String dataSourceKey = element.getAsString();
+            if (dataSourceKey.length() > 0 && dataSourceKey.charAt(0) == PREFIX) {
+                JsonElement tempElement = dataProvider.getObject(dataSourceKey.substring(1), childIndex);
+                if (tempElement != null) {
+                    element = tempElement;
+                } else {
+                    Log.e(TAG, "Got null element for " + dataSourceKey);
+                }
+            }
+        }
+        return element;
+    }
+}
