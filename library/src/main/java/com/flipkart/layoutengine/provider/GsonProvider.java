3c9835f5eab817a9bfceecdf50babe0070df7cf7
Aditya Sharat <aditya.sharat@flipkart.com>
2015-04-15 14:12:38 +0530

    re-ident GsonProvider

--- library/src/main/java/com/flipkart/layoutengine/provider/GsonProvider.java
+++ library/src/main/java/com/flipkart/layoutengine/provider/GsonProvider.java
@@ -7,8 +7,7 @@ import com.google.gson.JsonElement;
  */
 public class GsonProvider implements Provider {
 
-    private static class ObjectHolder
-    {
+    private static class ObjectHolder {
         public String path;
         public JsonElement object;
     }
@@ -39,60 +38,62 @@ public class GsonProvider implements Provider {
         JsonElement el = null;
 
         for (String e : split) {
-            char lastChar = e.charAt(e.length()-1);
+            char lastChar = e.charAt(e.length() - 1);
 
-            if(']'==lastChar){
-                while (']'==lastChar) {
-                    String index = e.substring(e.lastIndexOf('[')+1, e.length()-1);
-                    if(index.equals(""))index = String.valueOf(childIndex);
+            if (']' == lastChar) {
+                while (']' == lastChar) {
+                    String index = e.substring(e.lastIndexOf('[') + 1, e.length() - 1);
+                    if (index.equals("")) index = String.valueOf(childIndex);
                     Integer iindex = Integer.valueOf(index);
-                    e = e.substring(0,e.lastIndexOf('['));
-                    if("".equals(e)){
-                        if(el==null){
+                    e = e.substring(0, e.lastIndexOf('['));
+                    if ("".equals(e)) {
+                        if (el == null) {
                             el = jObj.getAsJsonArray().get(iindex);
-                            lastChar = (char)0;
+                            lastChar = (char) 0;
                         } else {
                             el = el.getAsJsonArray().get(iindex);
-                            lastChar = (char)0;
+                            lastChar = (char) 0;
                         }
                     } else {
-                        lastChar = e.charAt(e.length()-1); // new last char
+                        lastChar = e.charAt(e.length() - 1); // new last char
 
                         // if next is object
-                        if(lastChar!=']'){
-                            if(el==null){
-                                if(jObj.getAsJsonObject().get(e) == null)return jObj.getAsJsonObject();
+                        if (lastChar != ']') {
+                            if (el == null) {
+                                if (jObj.getAsJsonObject().get(e) == null)
+                                    return jObj.getAsJsonObject();
                                 el = jObj.getAsJsonObject().get(e).getAsJsonArray().get(iindex);
                             } else {
-                                if(el.isJsonObject()){
+                                if (el.isJsonObject()) {
                                     el = el.getAsJsonObject().get(e).getAsJsonArray().get(iindex);
-                                } else if(el.isJsonArray()){
+                                } else if (el.isJsonArray()) {
                                     el = el.getAsJsonArray().get(iindex);
                                 }
                             }
                         } else { // next is array
-                            if(el==null){
-                                if(jObj.isJsonObject()){
-                                    String locale = e.substring(0,e.indexOf('['));
+                            if (el == null) {
+                                if (jObj.isJsonObject()) {
+                                    String locale = e.substring(0, e.indexOf('['));
                                     el = jObj.getAsJsonObject().get(locale).getAsJsonArray().get(iindex);
-                                } else if(jObj.isJsonArray()){
+                                } else if (jObj.isJsonArray()) {
                                     el = jObj.getAsJsonArray().get(iindex);
                                 }
                             } else {
-                                if(e.indexOf('[')>-1){
-                                    String locale = e.substring(0,e.indexOf('['));
+                                if (e.indexOf('[') > -1) {
+                                    String locale = e.substring(0, e.indexOf('['));
                                     el = el.getAsJsonObject().get(locale).getAsJsonArray().get(iindex);
                                 } else {
                                     el = el.getAsJsonArray().get(iindex);
-                                };
+                                }
+                                ;
 
                             }
                         }
                     }
                 }
-            } else{
+            } else {
                 // plain obj
-                if(el==null){
+                if (el == null) {
                     el = jObj.getAsJsonObject().get(e);
                 } else {
                     el = el.getAsJsonObject().get(e);
@@ -109,10 +110,10 @@ public class GsonProvider implements Provider {
 
 
     @Override
-    public Provider clone(){
+    public Provider clone() {
 
         try {
-            return (Provider)super.clone();
+            return (Provider) super.clone();
         } catch (CloneNotSupportedException e) {
             e.printStackTrace();
         }
