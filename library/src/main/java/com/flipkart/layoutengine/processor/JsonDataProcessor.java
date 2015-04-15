99470d8a24577c9d98080f58dc8c59de86bf7962
Aditya Sharat <aditya.sharat@flipkart.com>
2015-04-15 12:58:57 +0530

    #5 | changing JsonDataProcessor interface

--- library/src/main/java/com/flipkart/layoutengine/processor/JsonDataProcessor.java
+++ library/src/main/java/com/flipkart/layoutengine/processor/JsonDataProcessor.java
@@ -10,8 +10,8 @@ import com.google.gson.JsonObject;
 public abstract class JsonDataProcessor<E> extends AttributeProcessor<E> {
     @Override
     public void handle(ParserContext parserContext, String attributeKey, JsonElement attributeValue, E view) {
-        handleData(parserContext, attributeKey, attributeValue, view);
+        handleData(parserContext, attributeKey, attributeValue.getAsJsonObject(), view);
     }
 
-    public abstract void handleData(ParserContext parserContext, String attributeKey, Object attributeData, E view);
+    public abstract void handleData(ParserContext parserContext, String attributeKey, JsonObject attributeData, E view);
 }
