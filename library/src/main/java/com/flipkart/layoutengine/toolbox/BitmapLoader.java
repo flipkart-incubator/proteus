f6a4413d6cba82e837040443e869269bec8a4286
Aditya Sharat <aditya.sharat@flipkart.com>
2015-05-20 13:18:29 +0530

    Bitmap loader callback will now return the associated view.

--- library/src/main/java/com/flipkart/layoutengine/toolbox/BitmapLoader.java
+++ library/src/main/java/com/flipkart/layoutengine/toolbox/BitmapLoader.java
@@ -1,6 +1,7 @@
 package com.flipkart.layoutengine.toolbox;
 
 import android.graphics.Bitmap;
+import android.view.View;
 
 import com.flipkart.layoutengine.ImageLoaderCallBack;
 
@@ -12,15 +13,17 @@ import java.util.concurrent.Future;
 public interface BitmapLoader {
     /**
      * Useful for Synchronous download of bitmap. Use the returned {@link java.util.concurrent.Future#get()} to block on the download.
+     *
      * @param imageUrl
      * @return
      */
-    public Future<Bitmap> getBitmap(String imageUrl);
+    Future<Bitmap> getBitmap(String imageUrl, View view);
 
     /**
      * Useful for asynchronous download of bitmap.
+     *
      * @param imageUrl
      * @param imageLoaderCallBack
      */
-    public void getBitmap(String imageUrl, ImageLoaderCallBack imageLoaderCallBack);
+    void getBitmap(String imageUrl, ImageLoaderCallBack imageLoaderCallBack, View view);
 }
