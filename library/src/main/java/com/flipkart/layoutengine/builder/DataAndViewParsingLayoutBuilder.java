444364b05244c60db1653b12a2afa19b99543e19
Aditya Sharat <aditya.sharat@flipkart.com>
2015-06-01 14:42:01 +0530

    Adding method updateViewProvider to DataAndViewParsingLayoutBuilder

--- library/src/main/java/com/flipkart/layoutengine/builder/DataAndViewParsingLayoutBuilder.java
+++ library/src/main/java/com/flipkart/layoutengine/builder/DataAndViewParsingLayoutBuilder.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.view.ViewGroup;
 
 import com.flipkart.layoutengine.ParserContext;
+import com.flipkart.layoutengine.provider.GsonProvider;
 import com.flipkart.layoutengine.provider.Provider;
+import com.flipkart.layoutengine.toolbox.Utils;
 import com.flipkart.layoutengine.view.ProteusView;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -14,7 +16,7 @@ import com.google.gson.JsonObject;
  * {@link SimpleLayoutBuilder}
  */
 public class DataAndViewParsingLayoutBuilder extends DataParsingLayoutBuilder {
-    private final Provider viewProvider;
+    private Provider viewProvider;
 
     DataAndViewParsingLayoutBuilder(Context context, Provider viewProvider) {
         super(context);
@@ -37,6 +39,15 @@ public class DataAndViewParsingLayoutBuilder extends DataParsingLayoutBuilder {
             return createdView;
         }
         return super.onUnknownViewEncountered(context, viewType, parent, viewJsonObject, childIndex);
+    }
+
+    public void updateLayoutProvider(JsonObject newViewProvider) {
+        if (viewProvider != null && viewProvider.getRoot() != null) {
+            JsonObject viewProviderData = Utils.merge(viewProvider.getRoot().getAsJsonObject(), newViewProvider);
+            viewProvider.setRoot(viewProviderData);
+        } else {
+            viewProvider = new GsonProvider(newViewProvider);
+        }
     }
 
     private void onViewBuiltFromViewProvider(ProteusView createdView, String viewType,
