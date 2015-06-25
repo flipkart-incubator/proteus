245d3442a443ee12ad09bd5c2d9976d18aaf6caa
Aditya Sharat <aditya.sharat@flipkart.com>
2015-06-25 15:34:15 +0530

    remove unused files

--- TestApp/src/main/java/com/flipkart/networking/request/components/OnRequestErrorListener.java
+++
@@ -1,7 +0,0 @@
-package com.flipkart.networking.request.components;
-
-import com.flipkart.networking.request.BaseRequest;
-
-public interface OnRequestErrorListener<T> {
-	public abstract void onRequestError(BaseRequest<T> request, RequestError error);
-}
