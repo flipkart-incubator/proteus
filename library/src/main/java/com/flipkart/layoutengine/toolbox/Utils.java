671858cf12d45f7d31c9df6baa5a3dd45fbf3380
Aditya Sharat <aditya.sharat@flipkart.com>
2015-07-08 13:42:06 +0530

    Bump version

--- library/src/main/java/com/flipkart/layoutengine/toolbox/Utils.java
+++ library/src/main/java/com/flipkart/layoutengine/toolbox/Utils.java
@@ -21,7 +21,7 @@ import java.util.Set;
  */
 public class Utils {
     public static final String LIB_NAME = "proteus";
-    public static final String VERSION = "2.6.18-SNAPSHOT";
+    public static final String VERSION = "2.6.19-SNAPSHOT";
     public static final String TAG = getTagPrefix() + Utils.class.getSimpleName();
 
     public static JsonElement getElementFromData(String dataPath, JsonProvider dataProvider, int childIndex)
