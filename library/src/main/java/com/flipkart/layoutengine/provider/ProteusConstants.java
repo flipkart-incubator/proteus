5f92699dac7f5a17582b10daff7e3d30b8f35426
Aditya Sharat <aditya.sharat@flipkart.com>
2015-09-21 19:01:55 +0530

    re-indent

--- library/src/main/java/com/flipkart/layoutengine/provider/ProteusConstants.java
+++ library/src/main/java/com/flipkart/layoutengine/provider/ProteusConstants.java
@@ -9,24 +9,33 @@ import java.util.regex.Pattern;
  * Contains data binding constants
  */
 public class ProteusConstants {
+
     public static final String TYPE = "type";
     public static final String ID = "if";
     public static final String TAG = "tag";
+
     public static final String CHILDREN = "children";
     public static final String CHILD_TYPE = "childType";
+
     public static final String STYLE = "style";
     public static final String STYLE_DELIMITER = "\\.";
+
     public static final String DATA_CONTEXT = "dataContext";
     public static final String CHILD_DATA_CONTEXT = "childDataContext";
+
     public static final String DATA_VISIBILITY = "data";
     public static final String DATA_NULL = "null";
+
     public static final Character DATA_PREFIX = '$';
     public static final Character REGEX_PREFIX = '~';
     public static final Pattern REGEX_PATTERN = Pattern.compile("\\{\\{(\\S+?)\\}\\}\\$\\((.+?)\\)|\\{\\{(\\S+?)\\}\\}");
+
     public static final String DATA_PATH_DELIMITER = "\\.|\\[|\\]";
     public static final String DATA_PATH_SIMPLE_DELIMITER = "\\.";
+
     public static final String CHILD_INDEX_REFERENCE = "$index";
     public static final String ARRAY_DATA_LENGTH_REFERENCE = "$length";
     public static final String ARRAY_DATA_LAST_INDEX_REFERENCE = "$last";
+
     public static final Set<String> ATTRIBUTES_TO_IGNORE = new HashSet<>(Arrays.asList(new String[]{"android", "dataContext"}));
 }
