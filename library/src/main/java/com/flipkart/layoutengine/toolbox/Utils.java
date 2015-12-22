592cb935c889f1391143e497b21146e7303a5901
Aditya Sharat <aditya.sharat@flipkart.com>
2015-12-22 15:26:05 +0530

    bump version

--- library/src/main/java/com/flipkart/layoutengine/toolbox/Utils.java
+++ library/src/main/java/com/flipkart/layoutengine/toolbox/Utils.java
@@ -27,7 +27,7 @@ import java.util.Map;
 public class Utils {
 
     public static final String LIB_NAME = "proteus";
-    public static final String VERSION = "3.0.6-SNAPSHOT";
+    public static final String VERSION = "3.1.0-SNAPSHOT";
     public static final String TAG_DEBUG = Utils.getTagPrefix() + "debug";
     public static final String TAG_ERROR = Utils.getTagPrefix() + "error";
 
