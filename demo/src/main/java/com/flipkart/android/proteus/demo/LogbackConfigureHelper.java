a20d294581934c669a7cf53f0ade7796ca93131b
Aditya Sharat <aditya.sharat@flipkart.com>
2016-07-14 12:53:01 +0530

    refactor log back configure helper

--- demo/src/main/java/com/flipkart/android/proteus/demo/LogbackConfigureHelper.java
+++ demo/src/main/java/com/flipkart/android/proteus/demo/LogbackConfigureHelper.java
@@ -18,29 +18,13 @@ import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 public class LogbackConfigureHelper {
 
     public static void configure() {
-        // reset the default context (which may already have been initialized)
-        // since we want to reconfigure it
         LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
         lc.reset();
-
-        //File file = new File(context.getFilesDir(), "main.log");
 
         PatternLayoutEncoder encoder = new PatternLayoutEncoder();
         encoder.setContext(lc);
         encoder.setPattern("%d{HH:mm:ss.SSS} [%thread] %-5level %logger - %msg%n");
         encoder.start();
-
-
-        /*FileAppender<ILoggingEvent> fileAppender = new FileAppender<ILoggingEvent>();
-        fileAppender.setContext(lc);
-        fileAppender.setFile(file.getAbsolutePath());
-        fileAppender.setEncoder(encoder);
-        fileAppender.start();*/
-
-        /*AsyncAppender asyncAppender = new AsyncAppender();
-        asyncAppender.setContext(lc);
-        asyncAppender.addAppender(fileAppender);
-        asyncAppender.start();*/
 
         LogcatAppender logcatAppender = new LogcatAppender();
         logcatAppender.setContext(lc);
@@ -48,9 +32,7 @@ public class LogbackConfigureHelper {
         logcatAppender.start();
 
         ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-        root.setLevel(Level.OFF); // level.OFF can be used for muting all logs
-        //root.addAppender(asyncAppender); //for logging to a file
-        root.addAppender(logcatAppender); //for logging to logcat
-
+        root.setLevel(Level.OFF);
+        root.addAppender(logcatAppender);
     }
 }
