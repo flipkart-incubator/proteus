bc699e545179894e44fdd9f0c5e7e2858760fa49
Aditya Sharat <aditya.sharat@flipkart.com>
2017-01-12 12:33:25 +0530

    Add Dimension as a value

---
+++ library/src/main/java/com/flipkart/android/proteus/Dimension.java
@@ -0,0 +1,186 @@
+/*
+ * Apache License
+ * Version 2.0, January 2004
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+ *
+ * Copyright (c) 2017 Flipkart Internet Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.flipkart.android.proteus;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v4.util.LruCache;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.ViewGroup;
+
+import com.flipkart.android.proteus.parser.ParseHelper;
+import com.flipkart.android.proteus.toolbox.ProteusConstants;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Dimension
+ *
+ * @author aditya.sharat
+ */
+
+public class Dimension extends Value {
+
+    public static final int DIMENSION_UNIT_STYLE_ATTR = -3;
+    public static final int DIMENSION_UNIT_RESOURCE = -2;
+    public static final int DIMENSION_UNIT_ENUM = -1;
+    public static final int DIMENSION_UNIT_PX = TypedValue.COMPLEX_UNIT_PX;
+    public static final int DIMENSION_UNIT_DP = TypedValue.COMPLEX_UNIT_DIP;
+    public static final int DIMENSION_UNIT_SP = TypedValue.COMPLEX_UNIT_SP;
+    public static final int DIMENSION_UNIT_PT = TypedValue.COMPLEX_UNIT_PT;
+    public static final int DIMENSION_UNIT_IN = TypedValue.COMPLEX_UNIT_IN;
+    public static final int DIMENSION_UNIT_MM = TypedValue.COMPLEX_UNIT_MM;
+
+    public static final String NULL = "null";
+    public static final String MATCH_PARENT = "match_parent";
+    public static final String FILL_PARENT = "fill_parent";
+    public static final String WRAP_CONTENT = "wrap_content";
+
+    public static final String PREFIX_DIMENSION = "@dimen/";
+    public static final String PREFIX_ATTRIBUTE = "?";
+
+    public static final String SUFFIX_PX = "px";
+    public static final String SUFFIX_DP = "dp";
+    public static final String SUFFIX_SP = "sp";
+    public static final String SUFFIX_PT = "pt";
+    public static final String SUFFIX_IN = "in";
+    public static final String SUFFIX_MM = "mm";
+
+    public static final Map<String, Integer> sDimensionsMap = new HashMap<>();
+    public static final Map<String, Integer> sDimensionsUnitsMap = new HashMap<>();
+    public static final Map<String, Integer> styleMap = new HashMap<>();
+    public static final Map<String, Integer> attributeMap = new HashMap<>();
+
+    static {
+        sDimensionsMap.put(MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        sDimensionsMap.put(FILL_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        sDimensionsMap.put(WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        sDimensionsUnitsMap.put(SUFFIX_PX, DIMENSION_UNIT_PX);
+        sDimensionsUnitsMap.put(SUFFIX_DP, DIMENSION_UNIT_DP);
+        sDimensionsUnitsMap.put(SUFFIX_SP, DIMENSION_UNIT_SP);
+        sDimensionsUnitsMap.put(SUFFIX_PT, DIMENSION_UNIT_PT);
+        sDimensionsUnitsMap.put(SUFFIX_IN, DIMENSION_UNIT_IN);
+        sDimensionsUnitsMap.put(SUFFIX_MM, DIMENSION_UNIT_MM);
+    }
+
+    public final float value;
+    public final int unit;
+
+    public Dimension(float value, int unit) {
+        this.value = value;
+        this.unit = unit;
+    }
+
+    private Dimension(String dimension, Context context) {
+        Integer parameter = sDimensionsMap.get(dimension);
+        float value;
+        int unit;
+
+        if (parameter != null) {
+            value = parameter;
+            unit = DIMENSION_UNIT_ENUM;
+        } else {
+            int length = dimension.length();
+            if (length < 2) {
+                value = 0;
+                unit = DIMENSION_UNIT_PX;
+            } else { // find the units and value by splitting at the second-last character of the dimension
+                Integer u = sDimensionsUnitsMap.get(dimension.substring(length - 2));
+                String stringValue = dimension.substring(0, length - 2);
+                if (u != null) {
+                    value = ParseHelper.parseFloat(stringValue);
+                    unit = u;
+                } else if (dimension.startsWith(PREFIX_DIMENSION)) { // check if dimension is a local resource
+                    try {
+                        value = context.getResources().getIdentifier(dimension, "dimen", context.getPackageName());
+                        unit = DIMENSION_UNIT_RESOURCE;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        if (ProteusConstants.isLoggingEnabled()) {
+                            Log.e("Proteus", "could not find a dimension with name " + dimension + ". Error: " + e.getMessage());
+                        }
+                        value = 0;
+                        unit = DIMENSION_UNIT_PX;
+                    }
+                } else if (dimension.startsWith(PREFIX_ATTRIBUTE)) { // check if dimension is an attribute value
+                    try {
+                        String[] dimenArr = dimension.substring(1, length).split(":");
+                        String style = dimenArr[0];
+                        String attr = dimenArr[1];
+                        Integer styleId = styleMap.get(style);
+                        if (styleId == null) {
+                            styleId = R.style.class.getField(style).getInt(null);
+                            styleMap.put(style, styleId);
+                        }
+                        Integer attrId = attributeMap.get(attr);
+                        if (attrId == null) {
+                            attrId = R.attr.class.getField(attr).getInt(null);
+                            attributeMap.put(attr, attrId);
+                        }
+                        TypedArray a = context.getTheme().obtainStyledAttributes(styleId, new int[]{attrId});
+                        value = a.getDimensionPixelSize(0, 0);
+                        unit = DIMENSION_UNIT_STYLE_ATTR;
+                    } catch (Exception e) {
+                        if (ProteusConstants.isLoggingEnabled()) {
+                            Log.e("Proteus", "could not find a dimension with name " + dimension + ". Error: " + e.getMessage());
+                        }
+                        value = 0;
+                        unit = DIMENSION_UNIT_PX;
+                    }
+
+                } else {
+                    value = 0;
+                    unit = DIMENSION_UNIT_PX;
+                }
+            }
+        }
+
+        this.value = value;
+        this.unit = unit;
+    }
+
+    public static Dimension valueOf(String dimension, Context context) {
+        if (null == dimension) {
+            return DimensionCache.cache.get(NULL);
+        }
+        Dimension d = DimensionCache.cache.get(dimension);
+        if (null == d) {
+            d = new Dimension(dimension, context);
+            DimensionCache.cache.put(dimension, d);
+        }
+        return d;
+    }
+
+    private static class DimensionCache {
+        private static final LruCache<String, Dimension> cache = new LruCache<>(64);
+
+        static {
+            cache.put(NULL, new Dimension(0, DIMENSION_UNIT_PX));
+        }
+    }
+
+    @Override
+    Value copy() {
+        return this;
+    }
+}
---
+++ library/src/test/java/com/flipkart/android/proteus/DimensionTest.java
@@ -0,0 +1,226 @@
+/*
+ * Apache License
+ * Version 2.0, January 2004
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+ *
+ * Copyright (c) 2017 Flipkart Internet Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.flipkart.android.proteus;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.view.ViewGroup;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
+
+/**
+ * DimensionTest
+ *
+ * @author aditya.sharat
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class DimensionTest {
+
+    @Mock
+    private Context context = mock(Context.class);
+    @Mock
+    private Resources resources = mock(Resources.class);
+    @Mock
+    private Resources.Theme theme = mock(Resources.Theme.class);
+    @Mock
+    private TypedArray typedArray = mock(TypedArray.class);
+
+    @Before
+    @After
+    public void before() {
+        reset(context);
+        reset(resources);
+        reset(resources, context);
+    }
+
+    @Test
+    public void valueOf_Null() throws Exception {
+        Dimension d = Dimension.valueOf(null, null);
+        assertThat(d.value, is(0f));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_PX));
+    }
+
+    @Test
+    public void valueOf_MatchParent() throws Exception {
+        Dimension d = Dimension.valueOf("match_parent", null);
+        assertThat((int) d.value, is(ViewGroup.LayoutParams.MATCH_PARENT));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_ENUM));
+    }
+
+    @Test
+    public void valueOf_WrapContent() throws Exception {
+        Dimension d;
+        d = Dimension.valueOf("wrap_content", null);
+        assertThat((int) d.value, is(ViewGroup.LayoutParams.WRAP_CONTENT));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_ENUM));
+    }
+
+    @Test
+    public void valueOf_Unit_Invalid() throws Exception {
+        Dimension d = Dimension.valueOf("abcd", null);
+        assertThat(d.value, is(0f));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_PX));
+
+        d = Dimension.valueOf("16q", null);
+        assertThat(d.value, is(0f));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_PX));
+
+        d = Dimension.valueOf("16", null);
+        assertThat(d.value, is(0f));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_PX));
+
+        d = Dimension.valueOf("1", null);
+        assertThat(d.value, is(0f));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_PX));
+    }
+
+    @Test
+    public void valueOf_Unit_None() throws Exception {
+        Dimension d = Dimension.valueOf("16", null);
+        assertThat(d.value, is(0f));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_PX));
+    }
+
+    @Test
+    public void valueOf_Unit_PX() throws Exception {
+        Dimension d = Dimension.valueOf("16px", null);
+        assertThat(d.value, is(16f));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_PX));
+    }
+
+    @Test
+    public void valueOf_Unit_DP() throws Exception {
+        Dimension d = Dimension.valueOf("16dp", null);
+        assertThat(d.value, is(16f));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_DP));
+    }
+
+    @Test
+    public void valueOf_Unit_SP() throws Exception {
+        Dimension d = Dimension.valueOf("16sp", null);
+        assertThat(d.value, is(16f));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_SP));
+    }
+
+    @Test
+    public void valueOf_Unit_PT() throws Exception {
+        Dimension d = Dimension.valueOf("16pt", null);
+        assertThat(d.value, is(16f));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_PT));
+    }
+
+    @Test
+    public void valueOf_Unit_IN() throws Exception {
+        Dimension d = Dimension.valueOf("16in", null);
+        assertThat(d.value, is(16f));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_IN));
+    }
+
+    @Test
+    public void valueOf_Unit_MM() throws Exception {
+        Dimension d = Dimension.valueOf("16mm", null);
+        assertThat(d.value, is(16f));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_MM));
+    }
+
+    @Test
+    public void valueOf_Unit_Negative() throws Exception {
+        Dimension d = Dimension.valueOf("-16dp", null);
+        assertThat(d.value, is(-16f));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_DP));
+    }
+
+    @Test
+    public void valueOf_Unit_Resource() throws Exception {
+        String input = "@dimen/padding";
+        String name = "a.b.c";
+        float resId = 1;
+
+        when(resources.getIdentifier(input, "dimen", name)).thenReturn((int) resId);
+        when(context.getResources()).thenReturn(resources);
+        when(context.getPackageName()).thenReturn(name);
+
+        Dimension d = Dimension.valueOf(input, context);
+        assertThat(d.value, is(resId));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_RESOURCE));
+    }
+
+    @Test
+    public void valueOf_Unit_Resource_Exception() throws Exception {
+        String input = "@dimen/undefined";
+
+        //noinspection unchecked
+        when(resources.getIdentifier(input, "dimen", context.getPackageName())).thenThrow(Exception.class);
+        when(context.getResources()).thenReturn(resources);
+
+        Dimension d = Dimension.valueOf(input, context);
+
+        assertThat(d.value, is(0f));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_PX));
+    }
+
+    @Test
+    public void valueOf_Unit_Attribute() throws Exception {
+        float value = 35f;
+        when(typedArray.getDimensionPixelSize(0, 0)).thenReturn((int) value);
+        when(theme.obtainStyledAttributes(anyInt(), new int[]{anyInt()})).thenReturn(typedArray);
+        when(context.getTheme()).thenReturn(theme);
+
+
+        Dimension d = Dimension.valueOf("?Theme_AppCompat:actionBarSize", context);
+
+        assertThat(d.value, is(35f));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_STYLE_ATTR));
+    }
+
+    @Test
+    public void valueOf_Unit_Attribute_Exception() throws Exception {
+        Dimension d = Dimension.valueOf("?undefined:undefined", context);
+        assertThat(d.value, is(0f));
+        assertThat(d.unit, is(Dimension.DIMENSION_UNIT_PX));
+    }
+
+    @Test
+    public void copy() throws Exception {
+        Dimension d = new Dimension(0, 0);
+        assertThat(d, is(d.copy()));
+    }
+
+    @Test
+    public void cache() throws Exception {
+        Dimension d1 = Dimension.valueOf("16dp", context);
+        Dimension d2 = Dimension.valueOf("16dp", context);
+
+        assertThat(d1, is(d2));
+    }
+}
