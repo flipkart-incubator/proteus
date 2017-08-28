dc98950d5f4ef1cc0e0d1f9bd88fd87adfdfd284
Aditya Sharat <aditya.sharat@flipkart.com>
2017-08-28 15:39:18 +0530

    Unit Tests: Binding.java

--- proteus-core/src/test/java/com/flipkart/android/proteus/value/BindingTest.java
+++ proteus-core/src/test/java/com/flipkart/android/proteus/value/BindingTest.java
@@ -31,6 +31,19 @@ import static org.hamcrest.core.Is.is;
  */
 public class BindingTest {
 
+    /*
+     * {
+     *   "a" : {
+     *     "b": {
+     *       "c": 10,
+     *       "d": true
+     *      }
+     *   }
+     *   "e": ["alpha", 2, false],
+     *   "f": null
+     * }
+     *
+     */
     public static ObjectValue data() {
         ObjectValue object = new ObjectValue();
 
@@ -38,13 +51,32 @@ public class BindingTest {
         ObjectValue b = new ObjectValue();
         Primitive c = new Primitive(10);
         Primitive d = new Primitive(true);
+        Array e = new Array();
+        e.add(new Primitive("alpha"));
+        e.add(new Primitive(2));
+        e.add(new Primitive(false));
 
         b.add("c", c);
         b.add("d", d);
         a.add("b", b);
         object.add("a", a);
+        object.add("e", e);
+        object.add("f", Null.INSTANCE);
+        object.add("g", new Array());
 
         return object;
+    }
+
+    @Test
+    public void is_binding() throws Exception {
+        assertThat(Binding.isBindingValue("@{a.b.c}"), is(true));
+    }
+
+    @Test
+    public void is_not_binding() throws Exception {
+        assertThat(Binding.isBindingValue("a.b.c"), is(false));
+        assertThat(Binding.isBindingValue("@{"), is(false));
+        assertThat(Binding.isBindingValue("@}"), is(false));
     }
 
     @Test
@@ -54,6 +86,125 @@ public class BindingTest {
         Value value = binding.evaluate(null, data(), 0);
 
         assertThat(value.getAsString(), is("10"));
+    }
+
+    @Test
+    public void to_string() throws Exception {
+        String string = "@{a.b.c}";
+        Binding binding = Binding.valueOf(string, null, null);
+
+        assertThat(binding.toString(), is(string));
+    }
+
+    @Test
+    public void evaluate_null_1() throws Exception {
+        Binding binding = Binding.valueOf("@{a.b.c}", null, null);
+
+        ObjectValue data = data();
+        ((Binding.DataBinding) binding).assign(Null.INSTANCE, data, 0);
+
+        Value value = binding.evaluate(null, data, 0);
+
+        assertThat(value.toString(), is("NULL"));
+    }
+
+    @Test
+    public void evaluate_null_2() throws Exception {
+        Binding binding = Binding.valueOf("@{a.b.c.d}", null, null);
+
+        Value value = binding.evaluate(null, data(), 0);
+
+        assertThat(value.toString(), is("NULL"));
+    }
+
+    @Test
+    public void evaluate_null_3() throws Exception {
+        Binding binding = Binding.valueOf("@{a.b[0]}", null, null);
+
+        Value value = binding.evaluate(null, data(), 0);
+
+        assertThat(value.toString(), is("NULL"));
+    }
+
+    @Test
+    public void evaluate_null_4() throws Exception {
+        Binding binding = Binding.valueOf("@{e.a.b}", null, null);
+
+        Value value = binding.evaluate(null, data(), 0);
+
+        assertThat(value.toString(), is("NULL"));
+    }
+
+    @Test
+    public void evaluate_null_5() throws Exception {
+        Binding binding = Binding.valueOf("@{f.a.b}", null, null);
+
+        Value value = binding.evaluate(null, data(), 0);
+
+        assertThat(value.toString(), is("NULL"));
+    }
+
+    @Test
+    public void evaluate_index() throws Exception {
+        Binding binding = Binding.valueOf("@{$index}", null, null);
+
+        Value value = binding.evaluate(null, data(), 0);
+
+        assertThat(value.toString(), is("0"));
+    }
+
+    @Test
+    public void evaluate_array_index() throws Exception {
+        Binding binding = Binding.valueOf("@{e[$index]}", null, null);
+
+        Value value = binding.evaluate(null, data(), 1);
+
+        assertThat(value.toString(), is("2"));
+    }
+
+    @Test
+    public void evaluate_array_invalid_index_1() throws Exception {
+        Binding binding = Binding.valueOf("@{e[8]}", null, null);
+
+        Value value = binding.evaluate(null, data(), 1);
+
+        assertThat(value.toString(), is("NULL"));
+    }
+
+    @Test
+    public void evaluate_array_invalid_index_2() throws Exception {
+        Binding binding = Binding.valueOf("@{e[$index]}", null, null);
+
+        Value value = binding.evaluate(null, data(), 8);
+
+        assertThat(value.toString(), is("NULL"));
+    }
+
+    @Test
+    public void evaluate_array_length() throws Exception {
+        Binding binding = Binding.valueOf("@{e[$length]}", null, null);
+
+        Value value = binding.evaluate(null, data(), 1);
+
+        assertThat(value.toString(), is("3"));
+    }
+
+    @Test
+    public void evaluate_array_last_1() throws Exception {
+        Binding binding = Binding.valueOf("@{e[$last]}", null, null);
+
+        Value value = binding.evaluate(null, data(), 1);
+
+        assertThat(value.toString(), is("false"));
+    }
+
+    @Test
+    public void evaluate_array_last_2() throws Exception {
+        Binding binding = Binding.valueOf("@{g[$last]}", null, null);
+
+        Value value = binding.evaluate(null, data(), 1);
+
+        assertThat(value.toString(), is("NULL"));
     }
 
     @Test
