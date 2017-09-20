5f05af59a846e18f87f7d362c8d024f1e21628b4
Aditya Sharat <adityasharat@gmail.com>
2017-09-20 15:39:26 +0530

    Unit Test | Dimension.java Coverage methods 92% lines 87%

---
+++ proteus-core/src/test/java/android/util/TypedValue.java
@@ -0,0 +1,15 @@
+package android.util;
+
+/**
+ * <p>
+ * Mocked TypedValue for testing
+ * </p>
+ *
+ * @author adityasharat
+ */
+public class TypedValue {
+
+    public static float applyDimension(int unit, float value, DisplayMetrics metrics) {
+        return value;
+    }
+}
