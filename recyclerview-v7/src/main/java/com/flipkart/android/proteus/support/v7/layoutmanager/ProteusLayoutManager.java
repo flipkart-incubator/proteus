2f47a0bb4624194df61f384c8bdcb649e4efdf68
Aditya Sharat <adityasharat@gmail.com>
2017-11-22 13:41:05 +0530

    create LayoutManagerFactory and ProteusLayoutManager.Builder interface

---
+++ recyclerview-v7/src/main/java/com/flipkart/android/proteus/support/v7/layoutmanager/ProteusLayoutManager.java
@@ -0,0 +1,39 @@
+/*
+ * Apache License
+ * Version 2.0, January 2004
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+ *
+ * Copyright (c) 2017 Flipkart Internet Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.flipkart.android.proteus.support.v7.layoutmanager;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+
+import com.flipkart.android.proteus.support.v7.widget.ProteusRecyclerView;
+import com.flipkart.android.proteus.value.ObjectValue;
+
+/**
+ * ProteusLayoutManager
+ *
+ * @author adityasharat
+ */
+public abstract class ProteusLayoutManager extends RecyclerView.LayoutManager {
+
+    public interface Builder<A extends ProteusLayoutManager> {
+        A create(@NonNull ProteusRecyclerView view, @NonNull ObjectValue config);
+    }
+
+}
