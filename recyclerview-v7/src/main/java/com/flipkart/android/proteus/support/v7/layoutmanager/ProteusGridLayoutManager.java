52750af9fc427de29ec2d0c2fbfd5e47c50dd967
raviranjan <ravi.ranjan@collegedunia.com>
2021-11-15 12:34:19 +0530

    custom grid layout added

---
+++ recyclerview-v7/src/main/java/com/flipkart/android/proteus/support/v7/layoutmanager/ProteusGridLayoutManager.java
@@ -0,0 +1,44 @@
+package com.flipkart.android.proteus.support.v7.layoutmanager;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.GridLayoutManager;
+
+import com.flipkart.android.proteus.support.v7.widget.ProteusRecyclerView;
+import com.flipkart.android.proteus.value.ObjectValue;
+
+public class ProteusGridLayoutManager extends GridLayoutManager {
+
+
+    private static final String ATTRIBUTE_COL = "numCols";
+    private static final String ATTRIBUTE_ORIENTATION = "orientation";
+    private static final String ATTRIBUTE_REVERSE_LAYOUT = "reverse";
+
+
+    public static final LayoutManagerBuilder<ProteusGridLayoutManager> BUILDER = new LayoutManagerBuilder<ProteusGridLayoutManager>() {
+
+        @NonNull
+        @Override
+        public ProteusGridLayoutManager create(@NonNull ProteusRecyclerView view, @NonNull ObjectValue config) {
+
+             int orientation = config.getAsInteger(ATTRIBUTE_ORIENTATION, GridLayoutManager.VERTICAL);
+             boolean reverseLayout = config.getAsBoolean(ATTRIBUTE_REVERSE_LAYOUT, false);
+
+             //todo get column from config attribute
+           // int col = config.getAsInteger(ATTRIBUTE_COL);
+
+            return new ProteusGridLayoutManager(view.getContext(), 2, orientation, reverseLayout);
+        }
+    };
+    
+    
+    public ProteusGridLayoutManager(Context context, int spanCount, int orientation, boolean reverseLayout) {
+        super(context, spanCount, orientation, reverseLayout);
+    }
+
+
+
+
+
+}
